466c7a2d2dfce2445e4f0fe612d0873b08df6876
lcp0430 <changping.lu@gmail.com>
2015-01-20 10:41:50 +0800

    add login file to control

---
+++ Philips Lighting Luminaries Choicesheet/login.Designer.cs
@@ -0,0 +1,172 @@
+﻿using System.Runtime.InteropServices;
+using System;
+using System.Text;
+using System.IO;
+
+namespace Philips_Lighting_Luminaries_Choicesheet
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        #region
+        [DllImport("kernel32")]//返回0表示失败，非0为成功
+        private static extern long WritePrivateProfileString(string section, string key,
+            string val, string filePath);
+
+        [DllImport("kernel32")]//返回取得字符串缓冲区的长度
+        private static extern long GetPrivateProfileString(string section, string key,
+            string def, StringBuilder retVal, int size, string filePath);
+        #endregion
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.textBoxPwd = new System.Windows.Forms.TextBox();
+            this.labelUserName = new System.Windows.Forms.Label();
+            this.labelPwd = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "登录";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(285, 176);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 56);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "取消";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(92, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "用户名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(113, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "密码";
+            // 
+            // textBoxUserName
+            // 
+            this.textBoxUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBoxUserName.Location = new System.Drawing.Point(215, 50);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(199, 36);
+            this.textBoxUserName.TabIndex = 4;
+            this.textBoxUserName.Enter += new System.EventHandler(this.textBoxFocusEnter);
+            this.textBoxUserName.Leave += new System.EventHandler(this.textBoxFocusLeave);
+            // 
+            // textBoxPwd
+            // 
+            this.textBoxPwd.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBoxPwd.Location = new System.Drawing.Point(215, 108);
+            this.textBoxPwd.Name = "textBoxPwd";
+            this.textBoxPwd.PasswordChar = '*';
+            this.textBoxPwd.Size = new System.Drawing.Size(199, 36);
+            this.textBoxPwd.TabIndex = 5;
+            this.textBoxPwd.Enter += new System.EventHandler(this.textBoxFocusEnter);
+            this.textBoxPwd.Leave += new System.EventHandler(this.textBoxFocusLeave);
+            // 
+            // labelUserName
+            // 
+            this.labelUserName.BackColor = System.Drawing.Color.White;
+            this.labelUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelUserName.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.labelUserName.Location = new System.Drawing.Point(218, 51);
+            this.labelUserName.Name = "labelUserName";
+            this.labelUserName.Size = new System.Drawing.Size(196, 33);
+            this.labelUserName.TabIndex = 6;
+            this.labelUserName.Text = "Anonymous";
+            this.labelUserName.Click += new System.EventHandler(this.label_Click);
+            // 
+            // labelPwd
+            // 
+            this.labelPwd.BackColor = System.Drawing.Color.White;
+            this.labelPwd.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelPwd.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.labelPwd.Location = new System.Drawing.Point(217, 119);
+            this.labelPwd.Name = "labelPwd";
+            this.labelPwd.Size = new System.Drawing.Size(197, 25);
+            this.labelPwd.TabIndex = 7;
+            this.labelPwd.Text = "*********";
+            this.labelPwd.Click += new System.EventHandler(this.label_Click);
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 290);
+            this.Controls.Add(this.labelPwd);
+            this.Controls.Add(this.labelUserName);
+            this.Controls.Add(this.textBoxPwd);
+            this.Controls.Add(this.textBoxUserName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "login";
+            this.Text = "login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxUserName;
+        private System.Windows.Forms.TextBox textBoxPwd;
+        private System.Windows.Forms.Label labelUserName;
+        private System.Windows.Forms.Label labelPwd;
+    }
+}
---
+++ Philips Lighting Luminaries Choicesheet/login.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Philips_Lighting_Luminaries_Choicesheet
+{
+    public partial class login : Form
+    {
+        public Form1.userInfo curUser;
+
+        public login(Form1 f)
+        {
+            InitializeComponent();
+            curUser = f.curUser;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string iniFilePath = Application.StartupPath + @"\conf.ini";
+
+            if (textBoxUserName.Text == String.Empty && textBoxPwd.Text == String.Empty)
+            {
+                curUser.name = "Anonymous";
+                curUser.group = Form1.userGrp.UG_OPERATOR;
+
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+
+            //验证密码 
+            else if (textBoxUserName.Text == "admin")
+            {
+
+                if (!File.Exists(iniFilePath))
+                    return;
+
+                StringBuilder temp = new StringBuilder(256);
+                if (GetPrivateProfileString("Admin", "Name", "", temp, 256, iniFilePath) != 0)
+                {
+                    if (temp.ToString() == "admin")
+                    {
+                        // Now get the pwd
+                        if (GetPrivateProfileString("Admin", "Password", "", temp, 256, iniFilePath) != 0)
+                        {
+                            if (temp.ToString() == textBoxPwd.Text)
+                            {
+                                curUser.name = "admin";
+                                curUser.group = Form1.userGrp.UG_ADMIN;
+
+                                this.DialogResult = DialogResult.OK;
+                                this.Close();
+                            }
+                        }
+                    }
+                }
+                
+            } 
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label_Click(object sender, EventArgs e)
+        {
+            if (sender.Equals(labelUserName))
+            {
+                textBoxUserName.Focus();
+            }
+            else if (sender.Equals(labelPwd))
+            {
+                textBoxPwd.Focus();
+            }
+        }
+
+        private void textBoxFocusEnter(object sender, EventArgs e)
+        {
+            if (sender.Equals(textBoxUserName))
+            {
+                labelUserName.Visible = false;
+            }
+            else if (sender.Equals(textBoxPwd))
+            {
+                labelPwd.Visible = false;
+            }
+        }
+
+        private void textBoxFocusLeave(object sender, EventArgs e)
+        {
+            if (sender.Equals(textBoxUserName))
+            {
+                if (textBoxUserName.Text.Length < 1)
+                    labelUserName.Visible = true;
+            }
+            else if (sender.Equals(textBoxPwd))
+            {
+                if(textBoxPwd.Text.Length < 1)
+                    labelPwd.Visible = true;
+            }
+        }
+    }
+}
